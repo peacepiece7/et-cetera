578dce156a3e170cdcacea7226e68a6cb655a4c2
peacepiece7 <peacepiece7@naver.com>
2024-08-26 23:53:51 +0900

    ci(@app/punk-record): ci test

--- apps/punk-record/src/contexts/usePostAreaContext.tsx
+++ apps/punk-record/src/contexts/usePostAreaContext.tsx
@@ -1,12 +1,5 @@
 "use client"
-import {
-  Dispatch,
-  SetStateAction,
-  createContext,
-  useContext,
-  useRef,
-  useState,
-} from "react"
+import { Dispatch, SetStateAction, createContext, useContext, useRef, useState } from "react"
 
 interface PostAreaSlideValueContextType {
   contentsRef: React.RefObject<HTMLElement>
@@ -21,35 +14,25 @@ interface PostAreaSlideActionContextType {
   setNext: Dispatch<SetStateAction<string>>
 }
 
-const PostAreaSlideValueContext = createContext<
-  PostAreaSlideValueContextType | undefined
->(undefined)
-const PostAreaSlideActionContext = createContext<
-  PostAreaSlideActionContextType | undefined
->(undefined)
+const PostAreaSlideValueContext = createContext<PostAreaSlideValueContextType | undefined>(undefined)
+const PostAreaSlideActionContext = createContext<PostAreaSlideActionContextType | undefined>(undefined)
 
 /**
  * 목록을 이동하면 포스트 페이지가 스르륵 이동하는 애니메이션을 제공하는 프로바이더입니다.
  *
  * 아래 컨텍스트 훅을 참고하세요.
  *
- * @see {@linkcode usePostAreaSlideValue}
- * @see {@linkcode usePostAreaSlideAction}
+ * @see {@link usePostAreaSlideValue}
+ * @see {@link usePostAreaSlideAction}
  */
-export function PostAreaSlideProvider({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export function PostAreaSlideProvider({ children }: { children: React.ReactNode }) {
   const [isWorking, setIsWorking] = useState(false)
   const [next, setNext] = useState("")
   const [isDone, setIsDone] = useState(false)
   const contentsRef = useRef<HTMLElement>(null)
 
   return (
-    <PostAreaSlideActionContext.Provider
-      value={{ setIsDone, setIsWorking, setNext }}
-    >
+    <PostAreaSlideActionContext.Provider value={{ setIsDone, setIsWorking, setNext }}>
       <PostAreaSlideValueContext.Provider
         value={{
           contentsRef,
@@ -67,9 +50,7 @@ export function PostAreaSlideProvider({
 export function usePostAreaSlideValue() {
   const context = useContext(PostAreaSlideValueContext)
   if (context === undefined) {
-    throw new Error(
-      "usePostAreaSlideValue must be used within a PostAreaProvider"
-    )
+    throw new Error("usePostAreaSlideValue must be used within a PostAreaProvider")
   }
   return context
 }
@@ -77,9 +58,7 @@ export function usePostAreaSlideValue() {
 export function usePostAreaSlideAction() {
   const context = useContext(PostAreaSlideActionContext)
   if (context === undefined) {
-    throw new Error(
-      "usePostAreaSlideAction must be used within a PostAreaProvider"
-    )
+    throw new Error("usePostAreaSlideAction must be used within a PostAreaProvider")
   }
   return context
 }
