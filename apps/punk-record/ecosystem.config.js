e95ac0aed6635113076aedecf9cccef140f6ab1e
peacepiece7 <peacepiece7@naver.com>
2024-08-27 10:57:04 +0900

    ci(@app/punk-recrod): blog 배포 경로 수정

--- apps/punk-record/ecosystem.config.js
+++ apps/punk-record/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "punk-record",
       script: "pnpm",
-      args: "run start",
+      args: "run start --filter @app/punk-record",
       watch: true, // 파일 변경 감지 및 자동 재시작
       env: {
         NODE_ENV: "development",
