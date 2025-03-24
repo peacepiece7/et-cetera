2fed8b616256cd6f8cc4febe4610a1d66194a544
peacepiece7 <peacepiece7@naver.com>
2025-03-24 22:19:07 +0900

    feat(@app/punk-record): tco 제거

--- apps/punk-record/src/components/navigation/NavigationContentForDesktop.tsx
+++ apps/punk-record/src/components/navigation/NavigationContentForDesktop.tsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
 import { useMedia } from "react-use"
 import { SearchArea } from "./SearchArea"
-import { Button } from "@repo/ui-shadcn/ui/button"
 
 export default function NavigationContentForDesktop({
   navChildren,
@@ -21,7 +20,7 @@ export default function NavigationContentForDesktop({
 
   const isWide = useMedia("(min-width: 768px)", true)
   const [isRender, setIsRender] = useState(false)
-  const [activeIdx, setActiveIdx] = useState(activeTab === "nav" ? 0 : 1)
+  const [activeIdx, setActiveIdx] = useState(0)
   const { navTree } = useNavAnimation(navChildren, pathname?.split("/"), [isWide, activeIdx])
 
   const { barRef, sideBarWidth, isDragging } = useResizeSidebar()
@@ -37,16 +36,16 @@ export default function NavigationContentForDesktop({
       className={`${isRender ? "flex" : "hidden"} flex-col relative h-auto col-start-1 col-end-2 row-start-2 row-end-4 group z-30 bg-white`}
     >
       <SearchArea />
-      <div className="flex justify-around mt-4">
-        <Button size="lg" onClick={() => setActiveIdx(0)}>
+      {/* <div className="flex justify-around mt-4"> */}
+      {/* <Button size="lg" onClick={() => setActiveIdx(0)}>
           목록
-        </Button>
-        {isPostPage && (
+        </Button> */}
+      {/* {isPostPage && (
           <Button size="lg" onClick={() => setActiveIdx(1)}>
             목차
           </Button>
-        )}
-      </div>
+        )} */}
+      {/* </div> */}
       <nav ref={navRef} className="relative h-full overflow-y-scroll z-10 pt-4" style={{ width: `${sideBarWidth}px` }}>
         <div className="pt-10 text-base">
           <div className={`${activeIdx === 0 ? "visible" : "hidden"}`}>{navTree}</div>
@@ -59,7 +58,7 @@ export default function NavigationContentForDesktop({
         className={`
           absolute w-4 h-full top-0 bottom-0 right-0 m-auto bg-slate-300 opacity-5 z-0
           transition-all ease-in-out cursor-col-resize
-          group-hover:opacity-100 group-hover:right-[-16px] duration-300 hover:bg-slate-400
+          group-hover:opacity-100 group-hover:right-[-3px] duration-300 hover:bg-slate-400
           ${isDragging ? "right-[-16px] bg-slate-400 opacity-100" : ""}
     `}
       />
